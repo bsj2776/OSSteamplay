054b036d1ba0702fcf70e0e5e3c2cdf8904c18a5
orioripaori <100837224+orioripaori@users.noreply.github.com>
2022-05-09 14:33:49 +0900

    일단 조금 수정함.

--- account.c
+++ account.c
@@ -8,6 +8,7 @@ int selectMenu(){
 	printf("2. 가계부 지우기\n");
 	printf("3. 가계부 수정\n");
 	printf("4. 가계부 읽기\n");
+	printf("0. 정지하기\n");
 	printf("원하는 메뉴를 입력하세요. =>");
 	scanf("%d",&menu);
 	return menu;
@@ -19,11 +20,11 @@ int addAccount(Account *a,int count){
 	char reason[100];
 	char dummy[10];
 	printf("몇년도 가계부인가요?\n");
-	scanf("%d",&a->year);
+	scanf("%d",&a[count].year);
 	printf("몇월달 가계부인가요?\n");
-	scanf("%d",&a->month);
+	scanf("%d",&a[count].month);
 	printf("날짜는? ");
-	scanf("%d",&a->date);
+	scanf("%d",&a[count].date);
 	fgets(dummy,10,stdin);
 	printf("내역은? ");
 	fgets(reason,100,stdin);
@@ -33,59 +34,60 @@ int addAccount(Account *a,int count){
 			}
 		break;
 	}
-	strcpy(a->reason,reason);
+	strcpy(a[count].reason,reason);
 	printf("수입은? ");
-	scanf("%d",&a->income);
+	scanf("%d",&a[count].income);
 	printf("지출은? ");
-	scanf("%d",&a->outcome);
+	scanf("%d",&a[count].outcome);
 	printf("완료했습니다.\n");
 	return 1;
 }
 
 int deleteAccount(Account *a,int count){
-	int check;
-    	int doubleCheck;
-    	readAccount(a,count);//일단 목록을 불러와요
-    	printf("\n");
-    	printf("날짜는(취소 :0)? ");
-    	scanf("%d",&check);
-    	if(check==0){
-        	return 0;
-    	}else{
-        	printf("정말로 삭제합니까? (삭제:1) ");
-        	scanf("%d",&doubleCheck);
-        	if(doubleCheck==1){
-			if(a->date==check){
-				a->date=-1;
+	int month, date;
+    int doubleCheck;
+    readAccount(a,count);//일단 목록을 불러와요
+    printf("\n");
+    printf("몇월인가요(취소 :0)? ");
+    scanf("%d",&month);
+	printf("몇일인가요(취소 :0)? ");
+	scanf("%d",&date);
+    if(month==0||date==0){
+		return 0;
+    }else{
+    	printf("정말로 삭제합니까? (삭제:1) ");
+    	scanf("%d",&doubleCheck);
+    	if(doubleCheck==1){
+			for(int i=0;i<count;i++){
+				if(a[i].date==date&&a[i].month==month){
+					a[i].date=-1;
+				}
+				printf("=> 삭제됨! \n");
+				return 1;
 			}
-            	printf("=> 삭제됨! \n");
-            	return 1;
-        	}else{
+			}else{
             		printf("취소하셨습니다.\n");
             	return 0;
         	}
     	}
 }
 
-void readAccount(Account a){
+void readAccount(Account *a,int count){
     printf("\n******************\n");
-    printf("%d",a[i].year);
-    printf(" %d",a[i].month);
-    printf(" %d",a[i].date);
-    printf(" %d",a[i].income);
-    printf(" %d",a[i].outcome);
-    printf(" %s\n",a[i].reason);
+    for(int i=0;i<count;i++){
+        if(a[i].date==-1){
+            continue;
+        }else{
+            printf("%2d ",i+1);
+            printf("%d",a[i].year);
+            printf(" %d",a[i].month);
+            printf(" %d",a[i].date);
+            printf(" %d",a[i].income);
+            printf(" %d ",a[i].outcome);
+            printf(" %s",a[i].reason);
+        }
+    }
     printf("\n");
-}
-
-void listAccount(Account *a, int count){
-	for(int i = 0; i < count; i++){
-		if(a[i].date == -1){
-			continue;
-		}
-		printf("%2d ", i+1);
-		readAccount(a[i]);
-	}
 }
 
 int updateAccount(Account *a){
--- main.c
+++ main.c
@@ -1,23 +1,24 @@
-#include "account.h"
+#include "account.c"
 
 int main(void){
-	Account a;
+	Account a[100];
 	int index;
-	int count;
+	int count=0;
   int menu;
 	while(1){
 		menu=selectMenu();
 		if(menu==1){
-			count += addAccount(&a,count);
+			count += addAccount(a,count);//가계부 생성
 		}else if(menu==2){
-			deleteAccount(&a,count);
+			deleteAccount(a,count);//가계부 삭제
 		}else if(menu==3){
-			updateAccount(&a);
+			updateAccount(a);//수정
 		}else if(menu==4){
-			readAccount(&a);
+			readAccount(a,count);//읽어오기
 		}
 		else if(menu==0){
 			printf("장비를 정지합니다");
 			break;
 		}
 	}
+}
