0628bc7920ae913ffc3002720a6f6b8be986f7ed
21800349_백승현 <bsj2776@naver.com>
2022-05-15 16:25:33 +0900

    main modify

--- main.c
+++ main.c
@@ -4,7 +4,10 @@ int main(void){
 	Account a[100];
 	int index;
 	int count=0;
-  int menu;
+  	int menu;
+
+	count = loadData(a);
+	index = count;
 	while(1){
 		menu=selectMenu();
 		if(menu==1){
@@ -15,6 +18,16 @@ int main(void){
 			updateAccount(a,count);//수정
 		}else if(menu==4){
 			readAccount(a,count);//읽어오기
+		}else if(menu==5){
+			saveData(a, count);
+		}else if(menu==6){
+			deleteMonth(a, count);
+		}else if(menu==7){
+			searchDate(a, count);
+		}else if(menu==8){
+			searchPayment(a, count);
+		}else if(menu==9){
+			searchTotalPay(a, count);
 		}
 		else if(menu==0){
 			printf("장비를 정지합니다");
